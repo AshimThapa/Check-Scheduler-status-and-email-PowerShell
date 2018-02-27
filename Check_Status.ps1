6f3d1c9382439bb2364718b26fe6f84c3c9b6042
AshimThapa <33416021+AshimThapa@users.noreply.github.com>
2018-02-27 15:10:52 +1100

    Add files via upload

---
+++ Check_Status.ps1
@@ -0,0 +1,48 @@
+﻿#check status of scheduler process_F07 or Process_F33
+#calcualte the duration of the process running from last time
+#if the process is stock the duration will be greater than two hours , hence it will send email
+#we can further add code to kill and restart the same scheduler or another.
+
+function Check_Scheduler
+{
+    param([int]$filename) 
+    if($filename -eq 33)
+        {
+            $Scheduler="Process_F33"
+        }
+    elseif($filename -eq 07)
+        {
+             $Scheduler="Process_F07"
+        }
+
+    $status=(Get-ScheduledTask | Where TaskName -EQ $Scheduler).State 
+
+#read from a text file
+    $startTime=(Get-Content -Path E:\.....\time.txt) 
+    $endTime = Get-Date -format HH:mm:ss
+#calculate time difference
+    $TimeDiff = New-TimeSpan $startTime $endTime
+    if ($TimeDiff.Hours -lt 0) 
+    {
+	    $Hrs = ($TimeDiff.Hours) + 23
+	    $Mins = ($TimeDiff.Minutes) + 59
+	    $Secs = ($TimeDiff.Seconds) + 59
+    }
+    else
+    {
+	    $Hrs = $TimeDiff.Hours
+	    $Mins = $TimeDiff.Minutes
+	    $Secs = $TimeDiff.Seconds }
+    $Difference = '{0:00}:{1:00}:{2:00}' -f $Hrs,$Mins,$Secs
+
+    if($status -eq "Running")
+        {
+            if ($Hrs -gt 2)
+            {
+                #Stop-ScheduledTask -TaskName $Scheduler
+                echo "Sending email"
+                . .\GroupEmail.ps1; Send-Mail -group 'IT' -bodypath 'E:\.....\Body.txt' -subject "$Scheduler scheduler Process Failed"
+            }
+        }
+
+}
---
+++ GroupEmail.ps1
@@ -0,0 +1,18 @@
+﻿
+#Reads the email id from xml file
+#can send multiple attachemnt of any type
+#can read the body content from test file
+#this script will eliminate batch email sending script(blat) and we dont need to edit email ids in each batch script individually,
+#just change it in xml.
+
+
+function Send-Mail{ param([string]$group,[string]$bodypath,[string]$subject)
+$from="admin@admin.com"
+$smtp="somesmtp.smtp.com"
+$body=Get-Content -path $bodypath | Out-String  
+
+#Read the xml file and typecast it to System.Xml.XmlDocument
+[Xml]$XmlData=Get-Content -path E:\.......\Email.xml
+$to = $XmlData.emaillist.group |Where-Object {$_.id -eq $group} |Select-Object -Expand emailid
+Send-MailMessage -From $from -To $to -Subject $subject -Body $body -SmtpServer $smtp
+ }
